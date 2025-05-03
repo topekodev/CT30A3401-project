9a49130c9cdff0f19a57f636c18490dfe5bf3e13
samppa13 <samuelkosola@gmail.com>
2025-05-03 17:28:32 +0300

    Fix

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -7,14 +7,14 @@ const router = express.Router()
 
 router.post('/register', async (request, response) => {
     try {
-        const existingUser = await User.findOne({email: request.body.email})
+        const existingUser = await User.findOne({username: request.body.username})
         if (existingUser) {
             response.status(403).json({message: 'Username already in use'})
         }
         const salt = bcrypt.genSaltSync(10)
         const hash = bcrypt.hashSync(request.body.password, salt)
         await User.create({
-            username: request.body.email,
+            username: request.body.username,
             password: hash
         })
         response.status(200).json({message: 'User registered successfully'})
@@ -25,14 +25,14 @@ router.post('/register', async (request, response) => {
 
 router.post('/login', async (request, response) => {
     try {
-        const user = await User.findOne({email: request.body.email})
+        const user = await User.findOne({username: request.body.username})
         if (!user) {
             response.status(401).json({message: 'Login failed'})
         }
         else if (bcrypt.compareSync(request.body.password, user.password)) {
             const jwtPayload = {
                 id: user._id,
-                email: user.email
+                username: user.username
             }
             const token =jwt.sign(jwtPayload, process.env.SECRET, {expiresIn: '1d'})
             response.status(200).json({success: true, token: token})

8f1d735d41781a8e9feb6a7b09485332fc1aa3be
samppa13 <samuelkosola@gmail.com>
2025-05-03 16:23:50 +0300

    Implement user login in backend

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const User = require('../models/User')
 
 const router = express.Router()
@@ -16,6 +18,28 @@ router.post('/register', async (request, response) => {
             password: hash
         })
         response.status(200).json({message: 'User registered successfully'})
+    } catch (error) {
+        response.status(500).json({error: 'Internal server error'})
+    }
+})
+
+router.post('/login', async (request, response) => {
+    try {
+        const user = await User.findOne({email: request.body.email})
+        if (!user) {
+            response.status(401).json({message: 'Login failed'})
+        }
+        else if (bcrypt.compareSync(request.body.password, user.password)) {
+            const jwtPayload = {
+                id: user._id,
+                email: user.email
+            }
+            const token =jwt.sign(jwtPayload, process.env.SECRET, {expiresIn: '1d'})
+            response.status(200).json({success: true, token: token})
+        }
+        else {
+            response.status(401).json({message: 'Login failed'})
+        }
     } catch (error) {
         response.status(500).json({error: 'Internal server error'})
     }
